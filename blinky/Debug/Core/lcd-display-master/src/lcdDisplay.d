a342730c46a34fa1c819204f80f7295ecd95476e
Pamela Fialho <pamelafialho2002@hotmail.com>
2025-05-11 23:24:29 -0300

    updateLcdFuncionando

--- blinky/Debug/Core/lcd-display-master/src/lcdDisplay.d
+++
@@ -1,6 +0,0 @@
-Core/lcd-display-master/src/lcdDisplay.o: \
- ../Core/lcd-display-master/src/lcdDisplay.c \
- ../Core/lcd-display-master/src/lcdDisplay.h \
- ../Core/lcd-display-master/src/platform/platform.h
-../Core/lcd-display-master/src/lcdDisplay.h:
-../Core/lcd-display-master/src/platform/platform.h:
